a21c1fdf58985a4523161e5c1c7e8b18935bea79
t-rolfin <rolfin.dc@gmail.com>
2021-04-12 20:02:29 +0300

    Created DbContext and repository for leads.

---
+++ src/crm.infrastructure/LeadDbContext.cs
@@ -0,0 +1,33 @@
+﻿using crm.domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace crm.infrastructure
+{
+    public class LeadDbContext : DbContext, IUnitOfWork
+    {
+        private readonly LeadConnString ConnectionString;
+
+        public LeadDbContext(LeadConnString connectionString)
+            :base()
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString.Value);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/crm.infrastructure/LeadRepository.cs
@@ -0,0 +1,37 @@
+﻿using crm.domain.Interfaces;
+using crm.domain.LeadAggregate;
+using Rolfin.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.infrastructure
+{
+    public class LeadRepository : ILeadRepository
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+
+        public LeadRepository(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public Result<bool> Create(Lead obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<Lead> GetLead(Guid leadId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<bool> Update(Lead obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
