7911f549207309f9bd1131484988ff24eb8cabf6
t-rolfin <rolfin.dc@gmail.com>
2021-04-12 19:58:18 +0300

    Changed the type of 'ILeadRepository' to interface.

--- src/crm.domain/Interfaces/ILeadRepository.cs
+++ src/crm.domain/Interfaces/ILeadRepository.cs
@@ -8,21 +8,7 @@ using System.Threading.Tasks;
 
 namespace crm.domain.Interfaces
 {
-    public class ILeadRepository : IUnitOfWork, IRepository<Lead>
+    public interface ILeadRepository : IRepository<Lead>
     {
-        public Result<bool> Create(Lead obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<Lead> GetResult(Guid leadId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<bool> Update(Lead obj)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
