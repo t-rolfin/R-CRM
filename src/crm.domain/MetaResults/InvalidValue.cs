9a50f9cc760c0f6070af808fb2d7b063e0290a1c
t-rolfin <rolfin.dc@gmail.com>
2021-04-12 15:31:03 +0300

    Added new MetaResult (InvalidValue).

---
+++ src/crm.domain/MetaResults/InvalidValue.cs
@@ -0,0 +1,18 @@
+﻿using Rolfin.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.domain.MetaResults
+{
+    public class InvalidValue : IMetaResult
+    {
+        public int Code => 6;
+
+        public string Name => "InvalidValue";
+
+        public string Message { get; set; }
+    }
+}
