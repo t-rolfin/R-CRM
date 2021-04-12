2f8faf95a1901dbf5881a300f993ef7a85fa0dd0
t-rolfin <rolfin.dc@gmail.com>
2021-04-12 15:28:58 +0300

    Added interfaces for repositories.

---
+++ src/crm.domain/Interfaces/ILeadRepository.cs
@@ -0,0 +1,28 @@
+﻿using crm.domain.LeadAggregate;
+using Rolfin.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.domain.Interfaces
+{
+    public class ILeadRepository : IUnitOfWork, IRepository<Lead>
+    {
+        public Result<bool> Create(Lead obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<Lead> GetResult(Guid leadId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<bool> Update(Lead obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/crm.domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using crm.domain.LeadAggregate;
+using Rolfin.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.domain.Interfaces
+{
+    public interface IRepository<T>
+        where T : IAggregateRoot
+    {
+        Result<Lead> GetResult(Guid leadId);
+        Result<bool> Create(T obj);
+        Result<bool> Update(T obj);
+    }
+}
