17a29a18ad9a9044c2eb4317a805296d6a857c75
t-rolfin <rolfin.dc@gmail.com>
2021-05-04 18:02:42 +0300

    Added hypermedia links.

--- src/crm.api/EndPoints/LeadDetails/LeadDetailsEndpoint.cs
+++ src/crm.api/EndPoints/LeadDetails/LeadDetailsEndpoint.cs
@@ -1,7 +1,10 @@
 ﻿using Ardalis.ApiEndpoints;
 using crm.common;
 using crm.common.DTOs;
+using crm.common.Utils;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
@@ -13,7 +16,7 @@ namespace crm.api.EndPoints.LeadDetails
 {
     public class LeadDetailsEndpoint : BaseAsyncEndpoint
         .WithRequest<Guid>
-        .WithResponse<LeadDetailsDto>
+        .WithResponse<LeadDetailsModel>
     {
 
         private readonly ILeadQueryRepository _leadQueryRepo;
@@ -27,13 +30,34 @@ namespace crm.api.EndPoints.LeadDetails
         [SwaggerOperation(
             Summary = "Get information for a specific lead.",
             Tags = new[] { "LeadEndpoint" }
-            )]
-        public override async Task<ActionResult<LeadDetailsDto>> HandleAsync(Guid id, CancellationToken cancellationToken = default)
+        )]
+        public override async Task<ActionResult<LeadDetailsModel>> HandleAsync(Guid id, CancellationToken cancellationToken = default)
         {
             var leadDetails = await _leadQueryRepo.GetDetails(id);
 
+            if (leadDetails != null)
+            {
+                leadDetails.Actions.Add(
+                    "addnote", 
+                    $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}/leads/{ leadDetails.Id }/notes/add"
+                    );
 
-            return leadDetails;
+                leadDetails.Actions.Add(
+                    "closelead",
+                    "");
+
+                leadDetails.Actions.Add(
+                    "updateprice",
+                    "");
+
+                leadDetails.Actions.Add(
+                    "updateclientinfo",
+                    "");
+
+                return Ok(leadDetails);
+            }
+
+            return NotFound();
         }
     }
 }
