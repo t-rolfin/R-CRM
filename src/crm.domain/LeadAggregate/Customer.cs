4b3e5b3441c9411b7f58cec097c077f8970e98d5
t-rolfin <rolfin.dc@gmail.com>
2021-05-04 17:43:24 +0300

    Added a method to update the customer data and removed LeadId.

--- src/crm.domain/LeadAggregate/Customer.cs
+++ src/crm.domain/LeadAggregate/Customer.cs
@@ -1,11 +1,13 @@
 ﻿using crm.domain.ValueObjects;
 using System;
+using System.Collections.Generic;
 
 namespace crm.domain.LeadAggregate
 {
     public class Customer : Entity<Guid>
     {
-        protected Customer() : base() { }
+        protected Customer() 
+            : base()  { }
 
         public Customer(Name name, string phoneNumber, string emailAddress)
         {
@@ -13,16 +15,23 @@ namespace crm.domain.LeadAggregate
             PhoneNumber = phoneNumber;
             EmailAddress = emailAddress;
         }
-
-        public Guid LeadId { get; init; }
-        public Name Name { get; init; }
-        public string PhoneNumber { get; init; }
-        public string EmailAddress { get; init; }
+        
+        public Name Name { get; protected set; }
+        public string PhoneNumber { get; protected set; }
+        public string EmailAddress { get; protected set; }
         public Address BillingAddress { get; protected set; }
+
 
         public void AsignBillingAddress(string street, int number,string city, string bloc, int apartment)
         {
             this.BillingAddress = new Address(street, number, city, bloc, apartment);
+        }
+
+        public void Update(Name name, string phoneNumbar, string emailAddress)
+        {
+            this.Name = name;
+            this.PhoneNumber = phoneNumbar;
+            this.EmailAddress = emailAddress;
         }
     }
 }
