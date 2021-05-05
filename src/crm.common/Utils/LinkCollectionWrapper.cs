9fdeeb2880f16774f3c945e980c4f41b6a9216b6
t-rolfin <rolfin.dc@gmail.com>
2021-05-05 20:19:23 +0300

    Added new helpers classes for hypermedia links.

---
+++ src/crm.common/Utils/LinkCollectionWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.common.Utils
+{
+    public class LinkCollectionWrapper<T> : LinkResourceBase
+        where T : LinkResourceBase
+    {
+        public List<T> Value { get; set; }
+            = new();
+
+        public LinkCollectionWrapper(List<T> value)
+        {
+            Value = value;
+        }
+    }
+}
