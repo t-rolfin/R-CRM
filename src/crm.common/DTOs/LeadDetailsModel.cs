aa91c5c518e21b1ee2f205e84596a41e1a73e61b
t-rolfin <rolfin.dc@gmail.com>
2021-05-05 20:20:42 +0300

    Updated DTOs to inherit from helpers classes.

--- src/crm.common/DTOs/LeadDetailsModel.cs
+++ src/crm.common/DTOs/LeadDetailsModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace crm.common.DTOs
 {
-    public class LeadDetailsModel : ActionList
+    public class LeadDetailsModel : LinkResourceBase
     {
 
         public LeadDetailsModel()
@@ -18,7 +18,7 @@ namespace crm.common.DTOs
         public DateTime CloseLeadDate { get;  set; }
         public DateTime CatchLead { get; set; }
         public string LeadProducts { get; set; }
-        public string LeadStage { get; set; }
+        public int LeadStage { get; set; }
         public string CloseStatus { get; set; }
         public string DelivaryAddress { get; set; }
         public decimal ProductsValue { get; set; }
